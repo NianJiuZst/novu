517eca580a80e1340f0034dab6cc57af6bad954d
George <djabarov.george@gmail.com>
2025-04-14 14:42:32 +0300

    fix(api): add organization repository to notifications count tests

--- apps/api/src/app/inbox/usecases/notifications-count/notifications-count.spec.ts
+++ apps/api/src/app/inbox/usecases/notifications-count/notifications-count.spec.ts
@@ -1,6 +1,6 @@
 import sinon from 'sinon';
 import { expect } from 'chai';
-import { MessageRepository, SubscriberRepository } from '@novu/dal';
+import { MessageRepository, OrganizationRepository, SubscriberRepository } from '@novu/dal';
 import { ChannelTypeEnum } from '@novu/shared';
 import { buildMessageCountKey, CachedQuery } from '@novu/application-generic';
 
@@ -15,12 +15,17 @@ describe('NotificationsCount', () => {
   let notificationsCount: NotificationsCount;
   let messageRepository: sinon.SinonStubbedInstance<MessageRepository>;
   let subscriberRepository: sinon.SinonStubbedInstance<SubscriberRepository>;
+  let organizationRepository: sinon.SinonStubbedInstance<OrganizationRepository>;
 
   beforeEach(() => {
     messageRepository = sinon.createStubInstance(MessageRepository);
     subscriberRepository = sinon.createStubInstance(SubscriberRepository);
-
-    notificationsCount = new NotificationsCount(messageRepository as any, subscriberRepository as any);
+    organizationRepository = sinon.createStubInstance(OrganizationRepository);
+    notificationsCount = new NotificationsCount(
+      messageRepository as any,
+      subscriberRepository as any,
+      organizationRepository as any
+    );
   });
 
   describe('execute', () => {
