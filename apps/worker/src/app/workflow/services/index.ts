30bf12847f8ecf18cf10fc6e0aea82eca9241e46
Dima Grossman <dima@grossman.io>
2025-08-14 20:19:39 +0300

    Update index.ts

--- apps/worker/src/app/workflow/services/index.ts
+++ apps/worker/src/app/workflow/services/index.ts
@@ -1,5 +1,4 @@
 export * from './active-jobs-metric.service';
 export * from './standard.worker';
-export * from './step-template-fetcher.service';
 export * from './subscriber-process.worker';
 export * from './workflow.worker';
